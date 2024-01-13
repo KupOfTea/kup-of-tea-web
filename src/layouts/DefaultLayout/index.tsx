9fbc5178c949c0bc2b9538a723f1c0eb15745808
shinyo17 <17shinyo17@gmail.com>
2024-01-14 07:22:35 +0900

    fix: Posthog config

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,25 +1,44 @@
 'use client'
 
-import { PostHogProvider } from 'posthog-js/react'
+import posthog from 'posthog-js'
+import { PostHogProvider, usePostHog } from 'posthog-js/react'
 import { OverlayProvider } from '@toss/use-overlay'
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useEffect } from 'react'
 import { RecoilRoot } from 'recoil'
 import { SWRConfig } from 'swr'
+import { usePathname, useSearchParams } from 'next/navigation'
+
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+  })
+}
 
 interface Props {
   children?: ReactNode
 }
 
-const options = {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-}
-
 export default function DefaultLayout({ children }: Props) {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const posthog = usePostHog()
+
+  // Track pageviews
+  useEffect(() => {
+    if (pathname && posthog) {
+      let url = window.origin + pathname
+      if (searchParams.toString()) {
+        url = url + `?${searchParams.toString()}`
+      }
+      posthog.capture('$pageview', {
+        $current_url: url,
+      })
+    }
+  }, [pathname, searchParams, posthog])
+
   return (
-    <PostHogProvider
-      apiKey={process.env.NEXT_PUBLIC_POSTHOG_KEY}
-      options={options}
-    >
+    <PostHogProvider client={posthog}>
       <RecoilRoot>
         <SWRConfig
           value={{ revalidateIfStale: false, revalidateOnFocus: false }}
