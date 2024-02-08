83d26d88e357a67f1241425d236d73d6f21483d0
shinyo17 <17shinyo17@gmail.com>
2024-02-09 04:37:16 +0900

    refactor: Make New File For Navigation Grid

--- src/components/NavigationModal/index.tsx
+++ src/components/NavigationModal/index.tsx
@@ -1,13 +1,8 @@
 'use client'
 
-import uuid from 'react-uuid'
-import Image from 'next/image'
-import { useQuery } from '@supabase-cache-helpers/postgrest-swr'
-import { useRouter } from 'next/navigation'
-
-import client from '@/client/base'
-import { Team } from '@/types/team'
 import { useState } from 'react'
+
+import NavigationGrid from '../NavigationGrid'
 
 interface Props {
   isOpen: boolean
@@ -66,55 +61,5 @@ export default function NavigationModal({ isOpen, close }: Props) {
         </div>
       </div>
     )
-  )
-}
-
-interface GridProps {
-  gender: string
-  close: () => void
-}
-
-function NavigationGrid({ gender, close }: GridProps) {
-  const router = useRouter()
-  const { data } = useQuery(
-    client
-      .from('teams')
-      .select('name, ticker, logo')
-      .eq('gender', gender)
-      .order('id', { ascending: true }),
-    {
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false,
-    },
-  )
-
-  const teams: Team[] = data || []
-
-  const handleClick = (team: Team) => {
-    router.push(`/${team.ticker}`)
-    close()
-  }
-
-  return (
-    <div className="grid grid-cols-4">
-      {teams.map((team, idx) => {
-        return (
-          <button
-            key={uuid()}
-            onClick={() => handleClick(team)}
-            className="flex flex-col items-center p-4 space-y-2 cursor-pointer justify-center"
-          >
-            <Image
-              className="object-contain object-center aspect-square mb-1"
-              src={`${team.logo}`}
-              alt=""
-              width={1000}
-              height={1000}
-            />
-            <p className="text-gray-900 text-[7px] font-semibold">{`${team.name}`}</p>
-          </button>
-        )
-      })}
-    </div>
   )
 }
