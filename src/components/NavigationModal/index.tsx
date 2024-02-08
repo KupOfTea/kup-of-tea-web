c1c40a3999df41b792f9b714c676061855f29746
shinyo17 <17shinyo17@gmail.com>
2024-02-09 04:31:55 +0900

    feat: Categorize Groups By Gender

--- src/components/NavigationModal/index.tsx
+++ src/components/NavigationModal/index.tsx
@@ -7,6 +7,7 @@ import { useRouter } from 'next/navigation'
 
 import client from '@/client/base'
 import { Team } from '@/types/team'
+import { useState } from 'react'
 
 interface Props {
   isOpen: boolean
@@ -14,18 +15,7 @@ interface Props {
 }
 
 export default function NavigationModal({ isOpen, close }: Props) {
-  const router = useRouter()
-  const { data } = useQuery(client.from('teams').select('name, ticker, logo').order('id', { ascending: true }), {
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-  })
-
-  const teams: Team[] = data || []
-
-  const handleClick = (team: Team) => {
-    router.push(`/${team.ticker}`)
-    close()
-  }
+  const [gender, setGender] = useState('boy')
 
   return (
     isOpen && (
@@ -51,31 +41,80 @@ export default function NavigationModal({ isOpen, close }: Props) {
                     폼 작성하러 가기
                   </a>
                 </div>
-                <div className="grid grid-cols-3">
-                  {teams.map((team, idx) => {
-                    return (
-                      <button
-                        key={uuid()}
-                        onClick={() => handleClick(team)}
-                        className="flex flex-col items-center p-4 space-y-2 cursor-pointer justify-center"
-                      >
-                        <Image
-                          className="object-contain object-center aspect-square mb-1 w-1/3"
-                          src={`${team.logo}`}
-                          alt=""
-                          width={1000}
-                          height={1000}
-                        />
-                        <p className="text-gray-900 text-[8px] font-semibold">{`${team.name}`}</p>
-                      </button>
-                    )
-                  })}
+                <div className="flex flex-row items-center">
+                  <button
+                    onClick={() => {
+                      setGender('boy')
+                    }}
+                    className="mx-3 text-sky-600 bg-white border border-sky-600 px-3 py-1.5 rounded-2xl text-sm font-bold"
+                  >
+                    보이그룹
+                  </button>
+                  <button
+                    onClick={() => {
+                      setGender('girl')
+                    }}
+                    className="mx-3 text-rose-600 bg-white border border-rose-600 px-3 py-1.5 rounded-2xl text-sm font-bold"
+                  >
+                    걸그룹
+                  </button>
                 </div>
+                <NavigationGrid gender={gender} close={close} />
               </div>
             </div>
           </div>
         </div>
       </div>
     )
+  )
+}
+
+interface GridProps {
+  gender: string
+  close: () => void
+}
+
+function NavigationGrid({ gender, close }: GridProps) {
+  const router = useRouter()
+  const { data } = useQuery(
+    client
+      .from('teams')
+      .select('name, ticker, logo')
+      .eq('gender', gender)
+      .order('id', { ascending: true }),
+    {
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    },
+  )
+
+  const teams: Team[] = data || []
+
+  const handleClick = (team: Team) => {
+    router.push(`/${team.ticker}`)
+    close()
+  }
+
+  return (
+    <div className="grid grid-cols-4">
+      {teams.map((team, idx) => {
+        return (
+          <button
+            key={uuid()}
+            onClick={() => handleClick(team)}
+            className="flex flex-col items-center p-4 space-y-2 cursor-pointer justify-center"
+          >
+            <Image
+              className="object-contain object-center aspect-square mb-1"
+              src={`${team.logo}`}
+              alt=""
+              width={1000}
+              height={1000}
+            />
+            <p className="text-gray-900 text-[7px] font-semibold">{`${team.name}`}</p>
+          </button>
+        )
+      })}
+    </div>
   )
 }
