cf362899c52152444f36f378dd9b84b0e7fd2390
keepwa1king <17shinyo17@gmail.com>
2024-02-24 07:41:26 +0900

    fix: Cannot Load External Image using html2canvas

--- src/components/SelectButton/index.tsx
+++ src/components/SelectButton/index.tsx
@@ -61,6 +61,7 @@ export default function SelectButton({ index, userId }: Props) {
           alt=""
           width={1000}
           height={1000}
+          crossOrigin="anonymous"
         />
       ) : (
         <svg
